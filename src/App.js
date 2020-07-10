812b01837c9f3c43381f2ff097728fdde32556c6
LawrenceTuano01 <lawrence.santos.tuano@gmail.com>
2020-07-10 15:12:01 +1200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div class="logo">M&W</div>
+    </div>
+  );
+}
+
+export default App;
